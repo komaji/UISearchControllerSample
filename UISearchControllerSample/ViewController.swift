cba314d250ccd74c0c2d128f71f65e69cf3c7797
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2017-09-03 21:33:00 +0900

    git init

---
+++ UISearchControllerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UISearchControllerSample
+//
+//  Created by KojimaTatsuya on 2017/09/03.
+//  Copyright © 2017年 KojimaTatsuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
