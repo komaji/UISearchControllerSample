63adb070b93a7a3115c07c19d8058fc3ec76c2b4
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2017-09-03 22:11:33 +0900

    show food categories

--- UISearchControllerSample/ViewControllers/SearchViewController.swift
+++ UISearchControllerSample/ViewControllers/SearchViewController.swift
@@ -10,4 +10,41 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    enum FoodCategory: String {
+        case pizza = "Pizza"
+        case humberger = "Humberger"
+        case sushi = "Sushi"
+    }
+    
+    let foodCategories: [FoodCategory] = [
+        .pizza,
+        .humberger,
+        .sushi
+    ]
+    
+    let tableViewCellReuesIdentifier = "SearchTableViewCell"
+    
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+            tableView.tableFooterView = UIView()
+        }
+    }
+    
+}
+
+// MARK: - UITableViewDatasource
+extension SearchViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foodCategories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: tableViewCellReuesIdentifier, for: indexPath)
+        cell.textLabel?.text = foodCategories[indexPath.row].rawValue
+        
+        return cell
+    }
+    
 }
