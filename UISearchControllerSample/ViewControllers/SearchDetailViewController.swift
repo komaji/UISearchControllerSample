ef878ba4fafaa82be75d814bbb3caee64ddb5376
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2017-09-03 22:16:37 +0900

    create view controller class for detail

---
+++ UISearchControllerSample/ViewControllers/SearchDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchDetailViewController.swift
+//  UISearchControllerSample
+//
+//  Created by KojimaTatsuya on 2017/09/03.
+//  Copyright © 2017年 KojimaTatsuya. All rights reserved.
+//
+
+import UIKit
+
+class SearchDetailViewController: UIViewController {
+    
+}
